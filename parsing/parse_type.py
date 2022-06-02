967cdfcbb55632c9446a11186a21fbde38715e29
farkon00 <59134591+farkon00@users.noreply.github.com>
2022-06-02 11:09:10 +0300

    Fixed typo in code

--- parsing/parse_type.py
+++ parsing/parse_type.py
@@ -4,7 +4,7 @@ from type_checking.types import *
 def parse_type(token: tuple[str, str], error):
     name = token[0]
     if name.startswith("*"):
-        return Ptr(parse_type((token[0][1:], token[1]), error)
+        return Ptr(parse_type((token[0][1:], token[1]), error))
     elif name == "int":
         return Int()
     elif name == "ptr":
