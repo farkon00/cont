0096bb7d200c85f794d0b4269bae9f33be2060de
farkon00 <sammer2016sammer@gmail.com>
2022-06-04 14:13:42 +0300

    Added test.py

---
+++ test.py
@@ -0,0 +1,46 @@
+from os import mkdir
+import subprocess
+import pytest
+
+tests = [
+    "arithmetics"
+]
+
+try:
+    mkdir("tests/results")
+except FileExistsError:
+    pass
+try:
+    mkdir("tests/temp")
+except FileExistsError:
+    pass
+
+@pytest.mark.parametrize("test_name", tests)
+def test(test_name):
+    with open(f"tests/{test_name}", "r") as f:
+        test = f.read()
+    
+    parts = test.split("\n:\n")
+
+    with open("tests/temp/code.cn", "w") as f:
+        f.write(parts[0])
+    with open("tests/temp/stdin", "w") as f:
+        if len(parts) > 2:
+            f.write(parts[2])
+    exp_stdout = parts[1]
+    if len(parts) > 3:
+        exp_stderr = parts[3]
+    else:
+        exp_stderr = ""
+
+    subprocess.run(["python3", "cont.py", "tests/temp/code.cn", "-i", "tests/temp/stdin", "-e",
+                    f"tests/results/{test_name}_stderr", "-o", f"tests/results/{test_name}_stdout", "-r"])
+
+    with open(f"tests/results/{test_name}_stdout", "r") as f:
+        stdout = f.read()
+
+    with open(f"tests/results/{test_name}_stderr", "r") as f:
+        stderr = f.read()
+
+    assert stdout == exp_stdout
+    assert stderr == exp_stderr
