819685253c46499a6c22fb7d3ef59c96bf996999
farkon00 <sammer2016sammer@gmail.com>
2022-08-14 12:36:39 +0300

    Prepared Config for adding config file

--- config.py
+++ config.py
@@ -1,26 +1,38 @@
 import argparse
 
 class Config:
+    DESCRIPTIONS: dict[str, str] = {
+        "program" : "The program to compile and optionally run",
+        "run" : "Run program after compilation",
+        "dump" : "Dump operations without compilation",
+        "dump_tokens" : "Dump tokens without parsing or compilating",
+        "o" : "The output executable file and name for .asm file",
+        "stdout" : "File to output stdout of complier and program",
+        "input" : "Stdin for program",
+        "error" : "Stderr for program",
+    }
+
+    BOOL_OPTIONS: dict[str, tuple[list[str], bool]] = {
+        "run" : (["-r", "-run"], False),
+        "dump" : (["-d", "-dump"], False),
+        "dump_tokens" : (["-dt", "-dump-tokens"], False),
+    }
+
     def __init__(self, argv):
         self.args = self.setup_args_parser().parse_args(argv[1:])
 
     def setup_args_parser(self):
         args_parser = argparse.ArgumentParser()
-        args_parser.add_argument("program", help="The program to compile and optionally run")
-        args_parser.add_argument("-o", "--out", default=None, dest="out", 
-            help="The output executable file and name for .asm file")
-        args_parser.add_argument("-r", "--run", action="store_true", default=False, dest="run", 
-            help="Run program after compilation")
-        args_parser.add_argument("--dump", action="store_true", default=False, dest="dump", 
-            help="Dump opeartions without compilation")
-        args_parser.add_argument("--dump-tokens", action="store_true", default=False, dest="dump_tokens", 
-            help="Dump tokens without parsing or compilating")
-        args_parser.add_argument("-stdo", "--stdout", dest="stdout", default=None, 
-            help="File to output stdout of complier and program")
-        args_parser.add_argument("-i", "--input", dest="input", default=None, 
-            help="Stdin for program")
-        args_parser.add_argument("-e", "--error", dest="error", default=None, 
-            help="Stderr for program")
+        
+        args_parser.add_argument("program", help=self.DESCRIPTIONS["program"])
+
+        args_parser.add_argument("-o", "--out", default=None, dest="out", help=self.DESCRIPTIONS["o"])
+        args_parser.add_argument("-stdo", "--stdout", dest="stdout", default=None, help=self.DESCRIPTIONS["stdout"])
+        args_parser.add_argument("-i", "--input", dest="input", default=None, help=self.DESCRIPTIONS["input"])
+        args_parser.add_argument("-e", "--error", dest="error", default=None, help=self.DESCRIPTIONS["error"])
+
+        for name, i in self.BOOL_OPTIONS.items():
+            args_parser.add_argument(*i[0], action="store_true", default=i[1], dest=name, help=self.DESCRIPTIONS[name])
 
         return args_parser
 
