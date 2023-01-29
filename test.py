5000fbc4f8ab4874fd546fb1d36c9927057fb6af
farkon00 <sammer2016sammer@gmail.com>
2023-01-29 09:53:30 +0200

    Made tests run in parallel

--- test.py
+++ test.py
@@ -13,6 +13,9 @@ try:
 except FileExistsError:
     tests.remove("temp")
 
+if subprocess.getstatusoutput("fasm -v")[0] == 0: # Check if fasm is installed
+    print("Please install Flat Assembler (Fasm)")
+    exit(1)
 
 @pytest.mark.parametrize("test_name", tests)
 def test(test_name):
@@ -21,9 +24,9 @@ def test(test_name):
 
     parts = test.split("\n:\n")
 
-    with open("tests/temp/code.cn", "w") as f:
+    with open(f"tests/temp/code_{test_name}.cn", "w") as f:
         f.write(parts[0])
-    with open("tests/temp/stdin", "w") as f:
+    with open(f"tests/temp/stdin_{test_name}", "w") as f:
         if len(parts) > 2:
             f.write(parts[2])
     exp_stdout = parts[1]
@@ -32,25 +35,23 @@ def test(test_name):
     else:
         exp_stderr = ""
 
-    fasm = subprocess.getstatusoutput("fasm -v")
-
-    if fasm[0] == 0:
-        print("Please install Flat Assembler (Fasm)")
-
     subprocess.run(
         [
-            "python",
-            "cont.py",
-            "tests/temp/code.cn",
-            "-i",
-            "tests/temp/stdin",
-            "-e",
-            f"tests/results/{test_name}_stderr",
-            "--stdout",
-            f"tests/results/{test_name}_stdout",
+            "python", "cont.py", f"tests/temp/code_{test_name}.cn",
+            "-i", f"tests/temp/stdin_{test_name}",
+            "-e", f"tests/results/{test_name}_stderr",
+            "--stdout", f"tests/results/{test_name}_stdout",
             "-r",
         ]
     )
+
+    os.remove(f"tests/temp/code_{test_name}.cn")
+    os.remove(f"tests/temp/stdin_{test_name}")
+    try:
+        os.remove(f"tests/temp/code_{test_name}.asm")
+        os.remove(f"tests/temp/code_{test_name}")
+    except FileNotFoundError:
+        pass
 
     with open(f"tests/results/{test_name}_stdout", "r") as f:
         stdout = f.read()
