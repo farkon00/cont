d55ba7fc1d486eab793bec87b5134194ef7c814f
farkon00 <59134591+farkon00@users.noreply.github.com>
2023-08-06 23:30:34 +0300

    Made exports not supported for anything other that wat64

--- parsing/parsing.py
+++ parsing/parsing.py
@@ -837,6 +837,7 @@ def parse_token(token: str, ops: List[Op]) -> Union[Op, List[Op]]:
         State.imported_procs.append((name, path))
 
     elif token == "#export":
+        assert State.config.target == "wat64", "Current target does not support exports"
         name = safe_next_token("Expected a procedure name")[0]
         assert name in State.procs, f'Procedure "{name}" not found'
         State.used_procs.add(State.procs[name])
